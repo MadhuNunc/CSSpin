2d571b7327fa546063df79a82bffecb7cbe91ac7
apple <appleapple@apples-MacBook-Air.local>
2019-06-11 17:17:14 +0530

    Version changes

---
+++ CSSpin.framework/Headers/CSSpin.h
@@ -0,0 +1,20 @@
+//
+//  CSSpin.h
+//  CSSpin
+//
+//  Created by apple on 6/11/19.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CaptureViewController.h"
+
+//! Project version number for CSSpin.
+FOUNDATION_EXPORT double CSSpinVersionNumber;
+
+//! Project version string for CSSpin.
+FOUNDATION_EXPORT const unsigned char CSSpinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSSpin/PublicHeader.h>
+
+
